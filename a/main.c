733143bb3850de8919c4bfe5c9028234b8fba7ba
fclef978 <fclef978@icloud.com>
2016-09-13 12:56:30 +0900

    Update main.c

--- a/main.c
+++ a/main.c
@@ -2,26 +2,30 @@
 //2016.9/12(Mon)//
 #include<stdio.h>
 
+#define L_OFFSET 5
+#define PERIOD 5
+#define BIT_WIDTH 255
+
 int make_wait(int cycle);
 
 
 int main(void){
-	int cycle = 0, loop = 0, result = 0, surplus = 0, tmp = 255, tmp2 = 0, i;
+	int cycle = 0, loop = 0, result = 0, surplus = 0, tmp = BIT_WIDTH, tmp2 = 0, i;
 	
 	puts("please input time[us]");
 	scanf("%d", &cycle);
-	cycle *= 5;
+	cycle *= PERIOD;
 	
-	for(loop = 255; loop > 0; loop--){
-		result = (cycle - 5) / loop - 3;
-		surplus = (cycle - 5) % loop;
+	for(loop = BIT_WIDTH; loop > 0; loop--){
+		result = (cycle - L_OFFSET) / loop - 3;
+		surplus = (cycle - L_OFFSET) % loop;
 		if(tmp > surplus && result < 30 && result > 0){
 			tmp = surplus;
 			tmp2 = loop;
 		}
 	}
 	
-	result = (cycle - 5) / tmp2 - 3;
+	result = (cycle - L_OFFSET) / tmp2 - 3;
 	surplus = tmp;
 	
 	printf("all cycle = %d\nloop = %d\ncycle = %d\nsurplus = %d\n", cycle, tmp2, result, surplus);
